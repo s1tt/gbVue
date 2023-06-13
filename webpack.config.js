b95a980d92ebaa2148738b820e0a3c5bb8faec36
Alekseev Sergei <nec13@mail.ru>
2023-06-13 11:43:02 +0300

    task complete

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/scripts/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, './src/dist'),
     filename: 'bundle.js',
   },
   module: {
